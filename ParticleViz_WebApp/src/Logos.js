a0ac0eaf62ee06654231db6d6202c468c28cba17
Philippe Miron <philippemiron@gmail.com>
2022-03-11 00:04:04 -0500

    add alt text and remove constructor to remove all warnings at launch

--- ParticleViz_WebApp/src/Logos.js
+++ ParticleViz_WebApp/src/Logos.js
@@ -27,15 +27,13 @@ function RenderLogos(props){
                 <img src={coaps_logo} className="rounded" width="30px" height="30px" alt="COAPS"/>
             </a>
             {logos.map(d => (
-                <a className="navbar-brand" href={d['url']} key={d['key']} alt="">
-                    <img src={props.url+"//"+"data/"+d['img']} className="rounded" height="30px"/>
+                <a className="navbar-brand" href={d['url']} key={d['key']}>
+                    <img src={props.url+"/data/"+d['img']} className="rounded" height="30px" alt=""/>
                 </a> ))}
         </>
     )
 }
 class  Logos extends React.Component {
-    constructor(props) { super(props) }
-
     render() {
         // if(isMobile ||  window.innerWidth <= 1200) {
         if(isMobile ||  window.innerWidth <= 1200) {
