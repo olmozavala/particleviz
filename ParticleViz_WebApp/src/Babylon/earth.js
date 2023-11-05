e86205380cfa05bd6ad1e93d1c4fef4ada6064d2
anand-kamble <anandmk837@gmail.com>
2023-11-05 16:13:00 -0500

    Updated documentation

--- ParticleViz_WebApp/src/Babylon/earth.js
+++ ParticleViz_WebApp/src/Babylon/earth.js
@@ -8,20 +8,63 @@ import {
   VertexData,
 } from "@babylonjs/core";
 
+/**
+ * Represents a 3D Earth model with stars and particles.
+ * @class
+ */
 class Earth {
-  scene;
   /**
+   * The Babylon.js scene.
+   * @type {Scene}
+   */
+  scene;
+
+  /**
+   * The mesh representing the Earth.
    * @type {Mesh | undefined}
    */
   #EarthMesh;
+
+  /**
+   * The camera used for rendering the scene.
+   * @type {ArcRotateCamera}
+   */
   #camera;
+
+  /**
+   * The points cloud system for stars.
+   * @type {PointsCloudSystem}
+   */
   #stars;
+
+  /**
+   * The color of stars.
+   * @type {Color4}
+   */
   #starColor;
+
+  /**
+   * The points cloud system for particles.
+   * @type {PointsCloudSystem}
+   */
   particles;
+
+  /**
+   * The color of particles.
+   * @type {Color4}
+   */
   particleColor;
+
+  /**
+   * Creates an instance of the Earth class.
+   * @constructor
+   * @param {Engine} engine - The Babylon.js engine.
+   * @param {HTMLElement} canvas - The HTML canvas element.
+   */
   constructor(engine, canvas) {
     this.scene = new Scene(engine);
     this.scene.clearColor = new Color4(0, 0, 0, 1);
+
     this.#camera = new ArcRotateCamera(
       "Camera",
       Math.PI / 2,
@@ -30,6 +73,7 @@ class Earth {
       Vector3.Zero(),
       this.scene
     );
+
     this.#starColor = new Color4(1, 1, 1, 1);
 
     this.#camera.lowerRadiusLimit = 2;
@@ -54,6 +98,11 @@ class Earth {
     this.particles.addPoints(20000, this.particleMaker.bind(this));
   }
 
+  /**
+   * Builds the mesh for stars and particles asynchronously.
+   * @async
+   * @returns {Promise<void>}
+   */
   async buildMesh() {
     await this.#stars.buildMeshAsync();
     await this.particles.buildMeshAsync();
@@ -70,6 +119,14 @@ class Earth {
     return;
   }
 
+  /**
+   * Converts Euler angles to Cartesian coordinates.
+   * @param {number} r - The radius.
+   * @param {number} phi - The azimuthal angle.
+   * @param {number} theta - The polar angle.
+   * @param {number} psi - The roll angle.
+   * @returns {Vector3} - The Cartesian coordinates.
+   */
   eulerToCartesian(r, phi, theta, psi) {
     let x = r * Math.cos(psi) * Math.cos(theta);
     let y = r * Math.sin(psi) * Math.cos(theta);
@@ -78,6 +135,11 @@ class Earth {
     return new Vector3(x, y, z);
   }
 
+  /**
+   * Generates the position and color of a star particle.
+   * @param {Particle} particle - The star particle.
+   * @returns {Particle} - The updated star particle.
+   */
   starMaker(particle) {
     particle.position = this.eulerToCartesian(
       25,
@@ -90,6 +152,11 @@ class Earth {
     return particle;
   }
 
+  /**
+   * Generates the position and color of a particle.
+   * @param {Particle} particle - The particle.
+   * @returns {Particle} - The updated particle.
+   */
   particleMaker(particle) {
     particle.position = Vector3.Zero();
     particle.color = this.particleColor;
@@ -97,6 +164,12 @@ class Earth {
     return particle;
   }
 
+  /**
+   * Updates the positions of particles in the point cloud system.
+   * @async
+   * @param {number[]} positions - The new positions of particles.
+   * @returns {Promise<void>}
+   */
   async updateParticles(positions) {
     var vertexData = new VertexData();
     vertexData.positions = positions;
--- ParticleViz_WebApp/src/Babylon/index.js
+++ ParticleViz_WebApp/src/Babylon/index.js
@@ -4,6 +4,10 @@ import "@babylonjs/loaders/glTF";
 import { Engine, Scene } from "@babylonjs/core";
 import Earth from "./earth";
 
+/**
+ * List of methods to be overwritten in the main application scope.
+ * @constant {string[]}
+ */
 const METHODS_TO_OVERWRITE = [
   "AddPointToBufferGeometry",
   "RenderPoints",
@@ -13,14 +17,52 @@ const METHODS_TO_OVERWRITE = [
   "updatePointsMaterial",
 ];
 
+/**
+ * Represents the main application controlling the 3D globe.
+ * @class
+ */
 class App {
+  /**
+   * The Babylon.js engine.
+   * @type {Engine}
+   */
   #engine;
+
+  /**
+   * The HTML canvas element.
+   * @type {HTMLCanvasElement}
+   */
   #canvas;
+
+  /**
+   * The HTML container element.
+   * @type {HTMLElement}
+   */
   #container;
+
+  /**
+   * The HTML navigation bar element.
+   * @type {HTMLElement | null}
+   */
   #navbar;
+
+  /**
+   * The main application scope.
+   * @type {Object}
+   */
   #mainAppScope;
+
+  /**
+   * The size of the trail.
+   * @type {number}
+   */
   #trailSize;
-  #previousPositions;
+
+  /**
+   * Creates an instance of the App class.
+   * @constructor
+   * @param {Object} mainAppScope - The main application scope.
+   */
   constructor(mainAppScope) {
     this.#mainAppScope = mainAppScope;
 
@@ -49,7 +91,6 @@ class App {
     this.#engine.hideLoadingUI();
 
     this.#trailSize = this.#mainAppScope.state.trail_size;
-    this.#previousPositions = [];
 
     this.Earth = new Earth(this.#engine, this.#canvas);
 
@@ -70,40 +111,69 @@ class App {
     });
   }
 
+  /**
+   * Adds a point to the buffer geometry based on latitude and longitude.
+   * @param {number} lat - The latitude.
+   * @param {number} lon - The longitude.
+   */
   AddPointToBufferGeometry(lat, lon) {
     const { x, y, z } = this.getCoordinatesFromlatlong(lat, lon);
-    // const { x, y, z } = this.getCoordinatesFromlatlong(18.51957, 73.85535);
     this.particlePositions.push(...[x, y, z]);
   }
 
+  /**
+   * Renders the points in the Earth scene.
+   */
   RenderPoints() {
     this.Earth.updateParticles(this.particlePositions);
   }
 
+  /**
+   * Updates the canvas on window resize.
+   */
   update3DCanvas() {
     this.#onWindowResize();
   }
 
+  /**
+   * Clears the points in the scene.
+   */
   clearThreeJSPoints() {
-    if (this.#previousPositions.length > 1) this.#previousPositions.shift();
-    this.#previousPositions.push(this.particlePositions);
     this.particlePositions = [];
   }
 
+  /**
+   * Updates the size of the trail.
+   */
   updateTrails() {
     this.#trailSize = this.#mainAppScope.state.trail_size;
   }
 
+  /**
+   * WIP
+   * Updates the points material.
+   */
   updatePointsMaterial() {
     return;
   }
 
+  /**
+   * Interpolates between two points.
+   * @param {Object} a - The first point.
+   * @param {Object} b - The second point.
+   * @param {number} frac - The interpolation factor.
+   * @returns {Object} - The interpolated point.
+   */
   interpolate(a, b, frac) {
     var nx = a.x + (b.x - a.x) * frac;
     var ny = a.y + (b.y - a.y) * frac;
     return { x: nx, y: ny };
   }
 
+  /**
+   * Handles window resize event.
+   * @private
+   */
   #onWindowResize() {
     this.#canvas.style.width = this.getCanvasSize().w;
     this.#canvas.style.height = this.getCanvasSize().h;
@@ -111,6 +181,12 @@ class App {
     this.#engine.resize();
   }
 
+  /**
+   * Converts latitude and longitude to 3D coordinates.
+   * @param {number} latitude - The latitude.
+   * @param {number} Longitude - The longitude.
+   * @returns {Object} - The 3D coordinates.
+   */
   getCoordinatesFromlatlong(latitude, Longitude) {
     const phi = (90 - latitude) * (Math.PI / 180);
     const theta = (Longitude + 180) * (Math.PI / 180);
@@ -122,6 +198,10 @@ class App {
     return { x, y, z };
   }
 
+  /**
+   * Gets the size of the canvas.
+   * @returns {Object} - The size of the canvas.
+   */
   getCanvasSize = () => {
     const getElementByXpath = (path) => {
       return this.#navbar
@@ -144,5 +224,11 @@ class App {
     };
   };
 }
+
+/**
+ * Initializes the Globe application.
+ * @param {Object} ReactScope - The main React application scope.
+ * @returns {App} - The Globe application instance.
+ */
 const Globe = (ReactScope) => new App(ReactScope);
 export default Globe;
